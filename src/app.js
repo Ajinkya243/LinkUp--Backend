11bff9c06319c4fff8b5dd812754c74304396661
Ajinkya243 <ajinkyagund2@gmail.com>
2025-12-14 16:15:27 +0530

    fix typo

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const{userRouter}=require("./routes/user")
 const cors=require('cors');
 app.use(express.json())
 app.use(cors({
-    origin:"http://localhost:5173/",
+    origin:"http://localhost:5173",
     credentials:true
 }));
 const cookieParser=require("cookie-parser")
